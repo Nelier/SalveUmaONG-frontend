9328342f48ab53c33c838ff9377894fdca9377d8
Nelier <victor-leonel@outlook.com>
2020-05-18 18:35:11 -0300

    Página home em funcionamento -Sem estilo -Beta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { FiLogIn } from "react-icons/fi";
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
-import api from "../../services/api";
+import api from '../../services/api';
 
-import "./styles.css";
+import './styles.css';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div class="home-container">
+      <header>
+        <span>Bem Vinda, ONG</span>
+      </header>
+
+      <div class="ongs-list-container">
+        <h1>Lista de ONGs</h1>
+
+        <ul class="listaONGs">
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
