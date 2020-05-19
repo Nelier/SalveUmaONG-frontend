cdccc7552ad3b5e7087d1973f285fc56fe9d4d71
Nelier <victor-leonel@outlook.com>
2020-05-19 00:15:07 -0300

    Estilização da Home iniciada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,10 @@ import api from '../../services/api';
 import './styles.css';
 
 export default function Home() {
+  const [ongs, setOngs] = useState('');
+
+  async function handlePopUp() {}
+
   return (
     <div class="home-container">
       <header>
@@ -16,8 +20,54 @@ export default function Home() {
       <div class="ongs-list-container">
         <h1>Lista de ONGs</h1>
 
-        <ul class="listaONGs">
-          <li></li>
+        <ul>
+          <li>
+            <strong class="ongTitle"> Organização dos Programadores</strong>
+            <strong class="ongLocal">São Paulo, SP</strong>
+            <p>
+              Estamos precisando de programadores voluntários para a criação de
+              uma API Rest
+            </p>
+            <button>Ler Mais</button>
+          </li>
+
+          <li>
+            <strong class="ongTitle"> Organização dos Programadores</strong>
+            <strong class="ongLocal">São Paulo, SP</strong>
+            <p>
+              Estamos precisando de programadores voluntários para a criação de
+              uma API Rest
+            </p>
+            <button>Ler Mais</button>
+          </li>
+
+          <li>
+            <strong class="ongTitle"> Organização dos Programadores</strong>
+            <strong class="ongLocal">São Paulo, SP</strong>
+            <p>
+              Estamos precisando de programadores voluntários para a criação de
+              uma API Rest
+            </p>
+            <button>Ler Mais</button>
+          </li>
+          <li>
+            <strong class="ongTitle"> Organização dos Programadores</strong>
+            <strong class="ongLocal">São Paulo, SP</strong>
+            <p>
+              Estamos precisando de programadores voluntários para a criação de
+              uma API Rest
+            </p>
+            <button>Ler Mais</button>
+          </li>
+          <li>
+            <strong class="ongTitle"> Organização dos Programadores</strong>
+            <strong class="ongLocal">São Paulo, SP</strong>
+            <p>
+              Estamos precisando de programadores voluntários para a criação de
+              uma API Rest
+            </p>
+            <button>Ler Mais</button>
+          </li>
         </ul>
       </div>
     </div>
