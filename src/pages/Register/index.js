8486af6515df63e72d5394c5248b1d5ea398e65b
Nelier <victor-leonel@outlook.com>
2020-05-21 04:53:37 -0300

    Integração ao Banco de Dados iniciada, Cadastro de ONGs funcionando.

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -7,8 +7,52 @@ import api from '../../services/api';
 import './styles.css';
 
 export default function Register() {
-  async function handleRegister() {
-    return null;
+  //User
+  const [email_user, setEmail_user] = useState('');
+  const [password, setPassword] = useState('');
+  const [user_name, setUser_name] = useState('');
+  //Adress
+  const [rua, setRua] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [cep, detCep] = useState('');
+  const [uf, setUf] = useState('');
+  //Ong
+  const [name, setName] = useState('');
+  const [name_fantasy, setName_fantasy] = useState('');
+  const [email_ong, setEmail_ong] = useState('');
+  const [description, setDescription] = useState('');
+  const [necessities, setNecessities] = useState('');
+  const [cel_number, setCel_number] = useState('');
+  const [cnpj, setCnpj] = useState('');
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    const data = {
+      email_user,
+      password,
+      user_name,
+      rua,
+      bairro,
+      cidade,
+      cep,
+      uf,
+      name,
+      name_fantasy,
+      email_ong,
+      description,
+      necessities,
+      cel_number,
+      cnpj,
+    };
+
+    try {
+      const respose = await api.post('/singup', data);
+
+      alert(`Seu ID de acesso: ${respose.data.id_user}`);
+    } catch (error) {
+      alert('Erro ao cadastrar: ' + error);
+    }
   }
 
   return (
@@ -30,6 +74,88 @@ export default function Register() {
       </header>
       <div className="register-container">
         <h1>Cadastre-se</h1>
+
+        <span>
+          Capriche em suas informações, elas serão usadas para que possíveis
+          voluntários entrem em contato
+        </span>
+        <br />
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome de usuário"
+            value={user_name}
+            onChange={(e) => setUser_name(e.target.value)}
+          />
+          <input
+            placeholder="Email"
+            value={email_user}
+            onChange={(e) => setEmail_user(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="Nome Fantasia"
+            value={name_fantasy}
+            onChange={(e) => setName_fantasy(e.target.value)}
+          />
+          <input
+            placeholder="Email da ONG"
+            value={email_ong}
+            onChange={(e) => setEmail_ong(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição da Causa"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <textarea
+            placeholder="Suas Necessidades"
+            value={necessities}
+            onChange={(e) => setNecessities(e.target.value)}
+          />
+          <input
+            placeholder="(xx)xxxxx-xxxx"
+            value={cel_number}
+            onChange={(e) => setCel_number(e.target.value)}
+          />
+          <input
+            placeholder="CNPJ"
+            value={cnpj}
+            onChange={(e) => setCnpj(e.target.value)}
+          />
+          <input
+            placeholder="Rua"
+            value={rua}
+            onChange={(e) => setRua(e.target.value)}
+          />
+          <input
+            placeholder="Bairro"
+            value={bairro}
+            onChange={(e) => setBairro(e.target.value)}
+          />
+          <input
+            placeholder="Cidade"
+            value={cidade}
+            onChange={(e) => setCidade(e.target.value)}
+          />
+          <input
+            placeholder="UF"
+            value={uf}
+            onChange={(e) => setUf(e.target.value)}
+          />
+          <button className="Button" type="submit">
+            Enviar
+          </button>
+        </form>
       </div>
     </div>
   );
